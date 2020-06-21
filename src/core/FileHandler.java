c17324ea2ee89b65b7ca26229bd3e186499d475e
Lotusdigital <gabriel.silva.010@acad.pucrs.br>
2020-06-21 19:46:17 -0300

    readFile and breakFile implementation

--- src/core/FileHandler.java
+++ src/core/FileHandler.java
@@ -1,28 +1,45 @@
 package src.core;
 
-import java.io.File;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Arrays;
+
 
 public class FileHandler {
     /*
     This class implements application's file handler
      */
 
+    File f;
+    File[] file_chunks;
+
     public FileHandler() {
-        /*
-        Constructor
-         */
+        f = this.f;
+        file_chunks = this.file_chunks;
     }
 
-    public void readFile(String filePath) {
-        /*
-        This method reads a file from parametrized path
-         */
+
+    public File readFile(String filePath) {
+            f = new File(filePath);
+            return f;
     }
 
-    public void breakFile(File f) {
-        /*
-        This method breaks a file into chunks of 512 bytes
-         */
+
+    public byte[][] breakFile(File f) throws IOException {
+
+        String content = Files.readString(f.toPath(), StandardCharsets.US_ASCII);
+        int size = (int) Math.ceil(content.length() / 508.0);
+        byte[][] messagesByte = new byte[size][512];
+        byte[] fileContent = Files.readAllBytes(f.toPath());
+        for (int i = 0; i < size; i++) {
+            byte[] aux = String.format("%04d", i).getBytes();
+            byte[] aux2 = Arrays.copyOfRange(fileContent,i * 508, Math.min((i + 1) * 508, fileContent.length));
+            System.arraycopy(aux, 0, messagesByte[i],0,aux.length);
+            System.arraycopy(aux2, 0, messagesByte[i], aux.length, aux2.length);
+        }
+
+        return messagesByte;
     }
 
     public void mountFile(File[] file_chunks) {
