f01c7d52e01ab455caf3c81bf975fd5fed67b0e6
Lotusdigital <gabriel.silva.010@acad.pucrs.br>
2020-06-24 17:53:42 -0300

    Checagem de md5 implementada, obrigado forum de 2008

--- src/core/FileHandler.java
+++ src/core/FileHandler.java
@@ -5,6 +5,9 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
+import java.security.DigestInputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.zip.CRC32;
 
@@ -116,9 +119,53 @@ public class FileHandler {
         Files.write(p, fileBytes);
     }
 
-    public void validateFile(File f) {
+    public void validateFile(File in, File out) throws NoSuchAlgorithmException, IOException {
         /*
          * This method validates a file using shasum or md5sum
          */
+
+        //Use MD5 algorithm
+        MessageDigest md5Digest = MessageDigest.getInstance("MD5");
+
+        //Get the checksum
+        String checksumIn = getFileChecksum(md5Digest, in);
+        String checksumOut = getFileChecksum(md5Digest, out);
+
+        //see checksum
+        if(checksumIn.equals(checksumOut)){
+            System.out.println("Deu bom gurizada");
+        }
+
+    }
+
+    public String getFileChecksum(MessageDigest digest, File file) throws IOException {
+        //Get file input stream for reading the file content
+        FileInputStream fis = new FileInputStream(file);
+
+        //Create byte array to read data in chunks
+        byte[] byteArray = new byte[1024];
+        int bytesCount = 0;
+
+        //Read file data and update in message digest
+        while ((bytesCount = fis.read(byteArray)) != -1) {
+            digest.update(byteArray, 0, bytesCount);
+        }
+        ;
+
+        //close the stream; We don't need it now.
+        fis.close();
+
+        //Get the hash's bytes
+        byte[] bytes = digest.digest();
+
+        //This bytes[] has bytes in decimal format;
+        //Convert it to hexadecimal format
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+        }
+
+        //return complete hash
+        return sb.toString();
     }
 }
